12644e37bef01d9104e6b2b6dfb98d7c8548f6db
CatMe0w <CatMe0w@live.cn>
2023-09-16 03:12:33 +0800

    Add authorization to Tests and make it work

--- tests/Exomemory.Tests/Tests.fs
+++ tests/Exomemory.Tests/Tests.fs
@@ -4,6 +4,7 @@ open System
 open System.IO
 open System.Net
 open System.Net.Http
+open Microsoft.Extensions.Configuration
 open Xunit
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
@@ -18,10 +19,20 @@ let createHost () =
     WebHostBuilder()
         .UseContentRoot(Directory.GetCurrentDirectory())
         .Configure(Action<IApplicationBuilder> Exomemory.Server.App.configureApp)
+        .ConfigureAppConfiguration(Exomemory.Server.App.configureAppConfiguration)
         .ConfigureServices(Action<IServiceCollection> Exomemory.Server.App.configureServices)
 
 let runTask task =
     task |> Async.AwaitTask |> Async.RunSynchronously
+
+let authorize (username: string) (password: string) (client: HttpClient) =
+    let authBase64 =
+        $"%s{username}:%s{password}"
+        |> Text.Encoding.UTF8.GetBytes
+        |> Convert.ToBase64String
+
+    client.DefaultRequestHeaders.Authorization <- Headers.AuthenticationHeaderValue("Basic", authBase64)
+    client
 
 let httpGet (path: string) (client: HttpClient) = path |> client.GetAsync |> runTask
 
@@ -31,7 +42,10 @@ let isStatus (code: HttpStatusCode) (response: HttpResponseMessage) =
 
 let ensureSuccess (response: HttpResponseMessage) =
     if not response.IsSuccessStatusCode then
-        response.Content.ReadAsStringAsync() |> runTask |> failwithf "%A"
+        // Since we don't have any content for non-successful responses, we just fail with the status code description
+        response.StatusCode.ToString() |> failwithf "%A"
+        
+        // response.Content.ReadAsStringAsync() |> runTask |> failwithf "%A"
     else
         response
 
@@ -51,18 +65,27 @@ let ``Route /overview returns a Room list and a Message list`` () =
     use server = new TestServer(createHost ())
     use client = server.CreateClient()
 
+    let settings = server.Host.Services.GetService<IConfiguration>()
+    let username = settings["Authentication:Username"]
+    let password = settings["Authentication:Password"] // XXX
+
     client
+    |> authorize username password
     |> httpGet "/overview"
-    |> isStatus HttpStatusCode.Unauthorized // TODO
-    // |> ensureSuccess
-    // |> readText
-    // |> shouldContain "\"rooms\":"
+    |> ensureSuccess
+    |> readText
+    |> shouldContain "\"rooms\":" // TODO
 
 [<Fact>]
 let ``Route which doesn't exist returns 404 Page not found`` () =
     use server = new TestServer(createHost ())
     use client = server.CreateClient()
 
+    let settings = server.Host.Services.GetService<IConfiguration>()
+    let username = settings["Authentication:Username"]
+    let password = settings["Authentication:Password"]
+
     client
+    |> authorize username password
     |> httpGet "/route/which/does/not/exist"
     |> isStatus HttpStatusCode.NotFound
